f95dda1665be579ab0b6ad77658378d4d1640be5
alosslessdev <ppaanncchhoo507@gmail.com>
2026-06-23 00:15:23 -0500

    remove unnecessary webapp section and make keyword blocker case insensitive

--- app/src/main/java/neth/iecal/curbox/data/models/WebApp.kt
+++
@@ -1,9 +0,0 @@
-package neth.iecal.curbox.data.models
-
-import java.util.UUID
-
-data class WebApp(
-    val id: String = UUID.randomUUID().toString(),
-    val name: String,
-    val url: String
-)
