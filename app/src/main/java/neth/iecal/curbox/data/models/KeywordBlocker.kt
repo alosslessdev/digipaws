86598380a89ad024493042326dec26be0403feda
alosslessdev <ppaanncchhoo507@gmail.com>
2026-07-04 22:32:23 -0500

    keyword blocker: make search-recursively actu

--- app/src/main/java/neth/iecal/curbox/data/models/KeywordBlocker.kt
+++ app/src/main/java/neth/iecal/curbox/data/models/KeywordBlocker.kt
@@ -5,7 +5,7 @@ data class KeywordBlocker(
     val keywordGroups: List<KeywordGroup> = emptyList(),
     val blockAllExceptSupported: Boolean = false,
     val redirectUrl: String = "https://curbox.life",
-    val searchRecursively: Boolean = true,
+    val searchRecursively: Boolean? = true,
     val ignoredApps: List<String> = emptyList(),
     val isTimeTrackingEnabled: Boolean = false,
     val keywordTimeLimits: Map<String, Int> = emptyMap(),
